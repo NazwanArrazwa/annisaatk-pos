60c1b98261de31615b359e990df9ba4ba0068041
NazwanArrazwa <fiensanz12345@gmail.com>
2023-08-25 18:29:57 +0800

    Test

--- application/views/admin/isi.php
+++ application/views/admin/isi.php
@@ -876,6 +876,7 @@ else if ($page == 'transaksiDetail') {
     <!-- Begin Page Content -->
     <div class="container-fluid">
 
+
         <!-- Page Heading -->
         <h1 class="h3 mb-2 text-gray-800">Detail Transaksi - <?php echo $detail[0]['kode_transaksi']; ?></h1>
 
